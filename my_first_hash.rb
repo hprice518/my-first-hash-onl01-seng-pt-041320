d3ce7e5ca8ba6841ff5a650434120737e0fe4e76
Harrison Price <hprice518@gmail.com>
2020-04-27 01:46:25 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -2,7 +2,6 @@ def my_hash
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
   my_hash = {"name" => "Harry", "age" => "26"} 
 
-  puts my_hash 
 end
 
 
